a783e43789e632989ea8b68251e73404755b2312
pauli-01 <mateuspaulistahnke@gmail.com>
2026-03-16 21:16:03 -0300

    imit:  Inicio de projeto

---
+++ src/main/java/README.md
@@ -0,0 +1,4 @@
+# Programa 01
+
+Desenvolvido utilizado JDK 1.8 e NetBeans 13
+
---
+++ src/main/java/com/mycompany/exercicio7/Exercicio7.java
@@ -0,0 +1,17 @@
+package com.mycompany.exercicio7;
+
+import javax.swing.JOptionPane;
+
+public class Exercicio7{
+    
+    public static void main(String[] args){
+        
+        String nome =JOptionPane.showInputDialog("digite seu nome:");
+        
+        JOptionPane.showMessageDialog(null, "Ola, " + nome);
+    }
+    
+}
+
+
+
